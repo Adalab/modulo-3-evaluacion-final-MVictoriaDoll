4686093d3910051d034bf7f21b00ba888656d25a
MVictoriaDoll <mvictoriadoll@gmail.com>
2024-08-19 12:39:50 +0200

    add with backup url

--- src/components/characters/characterItem.jsx
+++ src/components/characters/characterItem.jsx
@@ -12,10 +12,13 @@ function CharacterItem({ character }) {
         else {
             return 'otro';
         }
-    }
+    };
+
+    const imageUrl = character.image ? character.image : 'https://via.placeholder.com/210x295/ffffff/666666/?text=HarryPotter';
+
     return (
         <div className='details'>
-            <img className='character_img' src={character.image} alt={character.name} />
+            <img className='character_img' src={imageUrl} alt={character.name} />
             <h2 className='character_name'>{character.name}</h2>  <p className='character_house'>{translateSpecie(character.species)}</p>
         </div>
     );
